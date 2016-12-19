ccfec59e70958846693fd41828e8c12d1a59401f
liuzhengwang <liuzhengwang@bilibili.com>
2016-12-19 19:38:30 +0800

    my unique_ptr.h

--- example/unique_ptr_example_.cpp
+++ example/unique_ptr_example_.cpp
@@ -1,7 +1,14 @@
+#include <string>
+#include <iostream>
+
 #include "unique_ptr/unique_ptr.h"
 
 int main(int argc, char** argv)
 {
+    hinata::unique_ptr<std::string> up(new std::string("nico"));
+    //(*up)[0] = 'N'; // replace first character
+    //up->append("lai"); // append some characters
+    //std::cout << *up << std::endl; // print whole string
 
     return 0;
 }
