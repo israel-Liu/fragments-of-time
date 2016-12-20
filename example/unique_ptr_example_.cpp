267d03873cdec137fe05c08f59e40a590157313e
liuzhengwang <liuzhengwang@bilibili.com>
2016-12-20 18:39:22 +0800

    后续添加pointer 包裹 ptr_

--- example/unique_ptr_example_.cpp
+++ example/unique_ptr_example_.cpp
@@ -6,9 +6,9 @@
 int main(int argc, char** argv)
 {
     hinata::unique_ptr<std::string> up(new std::string("nico"));
-    //(*up)[0] = 'N'; // replace first character
-    //up->append("lai"); // append some characters
-    //std::cout << *up << std::endl; // print whole string
+    (*up)[0] = 'N';                 // replace first character
+    up->append("lai");              // append some characters
+    std::cout << *up << std::endl;  // print whole string
 
     return 0;
 }
--- unique_ptr/unique_ptr.h
+++ unique_ptr/unique_ptr.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <memory>
 
+#define noexcept _NOEXCEPT
+
 namespace hinata {
 
 template <typename T>
@@ -9,49 +11,86 @@ template <typename T>
     public:
         // Default constructor
         default_delete()
-        {
-
-        }
+        {}
 
     template <class U>
         default_delete(const default_delete<U>&)
-        {
-
-        }
+        {}
 
         void operator()(T* ptr) const
         {
             delete ptr;
         }
+
     private:
 
     };
 
-template <typename T, typename D = default_delete<T>> // D = default_delete<T>
+template <typename T, typename D = default_delete<T>>
     class unique_ptr
     {
     public:
-        unique_ptr()
-        {
+        typedef T   element_type;
+        typedef D   deleter_type;
 
-        }
-        explicit unique_ptr(T* ptr)
-        {
-            ptr_ = ptr;
-        }
+        unique_ptr() noexcept
+        {}
+
+        explicit unique_ptr(T* ptr) noexcept
+            : ptr_(ptr), del_(deleter_type())
+        {}
+
+        unique_ptr(T* ptr, D del) noexcept
+            : ptr_(ptr), del_(del)
+        {}
 
         // destructor
         ~unique_ptr()
         {
-            delete ptr_;
+            get_deleter()(ptr_);
         }
+
+        // observers
+        T& operator*() noexcept
+        {   // Requires: get() != nullptr.
+            return *get();
+        }
+
+        T* operator->() noexcept
+        {   // Requires: get() != nullptr.
+            return get();
+        }
+
+        T* get() noexcept
+        {
+            return ptr_;
+        }
+
+        deleter_type& get_deleter() noexcept
+        {
+            return del_;
+        }
+
+        const deleter_type& get_deleter() const noexcept
+        {
+            return del_;
+        }
+
+        explicit operator bool() const noexcept
+        {
+            return get() != nullptr;
+        }
+
+        // modifiers
+
 
         // disable copy from lvalue
         unique_ptr(const unique_ptr&) = delete;
         unique_ptr& operator=(const unique_ptr&) = delete;
 
     private:
-        T* ptr_;
+        T*  ptr_;
+        D   del_;
     };
 
 } // namespace hinata
