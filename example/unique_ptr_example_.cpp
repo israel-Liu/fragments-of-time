0b3b818a7f8f9f368e03bcbaf2fb635838c69119
liuzhengwang <liuzhengwang@bilibili.com>
2016-12-28 14:18:16 +0800

    Assign function

--- example/unique_ptr_example_.cpp
+++ example/unique_ptr_example_.cpp
@@ -25,10 +25,13 @@ int main(int argc, char** argv)
     wchar_t* s_n_c = new wchar_t[20];
     s_n_c = L"new_ten_char";
     std::basic_string<wchar_t> w_str(s_n_c);
+    std::basic_string<wchar_t> w_str_eq;
+    w_str_eq = w_str;
     std::cout << w_str.length() << std::endl;
 
     hinata::string<char> str_c("hinata::string<char>");
     hinata::string<wchar_t> str_wc(L"hinata::string<wchar_t>");
+    hinata::string<char> str_c_eq(str_c);
     //std::cout << str_c << std::endl;
 
     return 0;
