f0586e61b03001702238ae0906817cc1de324335
Israel.liu <Israel.liu@zoom.us>
2017-07-06 21:13:00 +0800

    observer_pattern.h

--- himawari/design_patterns/design_patterns/example_patterns/Source.cpp
+++ himawari/design_patterns/design_patterns/example_patterns/Source.cpp
@@ -2,6 +2,7 @@
 
 #include "decorator_pattern/decorator_pattern.h"
 #include "template_pattern/template_pattern.h"
+#include "observer_pattern.h"
 
 int main(int agrc, char** agrv)
 {
@@ -16,14 +17,30 @@ int main(int agrc, char** agrv)
     //cdb->SetTheComponent(cc);
     //cdb->DoJob();
 
-    std::cout << "***Template Pattern Demo***" << std::endl;
-    BasicEngineering* bs = new ComputerScience();
-    std::cout << "Computer Sc Papers:";
-    bs->Papers();
-    std::cout << std::endl;
-    bs = new Electronics();
-    std::cout << "Electronics Papers:";
-    bs->Papers();
+    //std::cout << "***Template Pattern Demo***" << std::endl;
+    //BasicEngineering* bs = new ComputerScience();
+    //std::cout << "Computer Sc Papers:";
+    //bs->Papers();
+    //std::cout << std::endl;
+    //bs = new Electronics();
+    //std::cout << "Electronics Papers:";
+    //bs->Papers();
+
+    ParaWeatherData* wdata = new ParaWeatherData;
+    CurrentConditionBoard* current_b = new CurrentConditionBoard(*wdata);
+    StatisticBoard* statistic_b = new StatisticBoard(*wdata);
+
+    wdata->SensorDataChange(10.2, 28.2, 1001);
+    wdata->SensorDataChange(12, 30.12, 1003);
+    wdata->SensorDataChange(10.2, 26, 806);
+    wdata->SensorDataChange(10.3, 35.9, 900);
+
+    wdata->RemoveOb(current_b);
+    wdata->SensorDataChange(100, 40, 1900);
+
+    delete statistic_b;
+    delete current_b;
+    delete wdata;
 
     return 0;
 }
