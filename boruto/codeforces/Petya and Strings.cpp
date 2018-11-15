bac622a22d3641789361044df51fa960f628fde1
Israel-liu <Israel.liu.theForger@gmail.com>
2018-11-15 14:50:40 +0800

    api design for cpp patterns

---
+++ boruto/codeforces/Petya and Strings.cpp
@@ -0,0 +1,17 @@
+/*
+Problem: http://codeforces.com/problemset/problem/112/A
+Date: 2017-12-01
+*/
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string s1, s2;
+	cin >> s1 >> s2;
+	cout << s1.compare(s2);
+	
+	return 0;
+}
